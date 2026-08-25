1bd6367aaceed6c9e508a2e0bf1321becd0a6a64
1224kang <cherrykang2003@gmail.com>
2026-08-25 15:38:09 +0900

    setting: 파일 구성 완료

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request,jsonify
+from bson import ObjectId
+from pymongo import MongoClient
+import os
+
+client=MongoClient(os.getenv("MONGO_URL"))
+db=client["jungle-capsule"]
+collection=db["items"]
+
+app = Flask(__name__)
+
+try:
+    client.admin.command("ping")
+    print("MongoDB 연결 성공")
+except Exception as e:
+    print("MongoDB 연결 실패")
+    print(e)
+
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0', port=5001, debug=True)
