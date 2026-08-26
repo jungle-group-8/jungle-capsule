0a0db4ef96900e7dacaefabe36cb3b4cc068dd16
1224kang <cherrykang2003@gmail.com>
2026-08-26 17:48:10 +0900

    merge conflict

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import Flask,render_template,request,jsonify,session,redirect,url_for
-from bson import ObjectId
-from pymongo import MongoClient
+from flask import Flask
 from routes.capsule import capsule_bp
-from routes.auth import auth_bp
+from dotenv import load_dotenv
 
 from services.auth import login #blueprint사용 login.py import
-from routes import capsule
 from database import client   #database에서 client  변수 가져옴
-
+from dotenv import load_dotenv
 import os
-from datetime import datetime
-import random
-from routes import capmaking,capsulelist,checkcap,deletecap,detailcpa,member,question
-
+from routes import auth,capmaking,capsulelist,checkcap,deletecap,detailcpa,member,question
+from services import imageupload
 
 
 
 app = Flask(__name__)
+
+load_dotenv()
+
 app.secret_key = os.getenv("SECRET_KEY")
+
 
 app.register_blueprint(capmaking.capmaking)
 app.register_blueprint(capsulelist.caplist)
@@ -26,9 +25,15 @@ app.register_blueprint(deletecap.deletecap)
 app.register_blueprint(detailcpa.detailcap)
 app.register_blueprint(member.members)
 app.register_blueprint(question.question)
+app.register_blueprint(imageupload.image_bp)
+app.register_blueprint(auth.auth_page_bp)
 
 app.register_blueprint(capsule_bp)
 app.register_blueprint(login.auth_bp)
+
+app.config["S3_ACCESS_KEY"] = os.getenv("S3_ACCESS_KEY")
+app.config["S3_SECRET_KEY"] = os.getenv("S3_SECRET_KEY")
+app.config["S3_BUCKET"] = os.getenv("S3_BUCKET")
 
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5001, debug=True)
--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 import os
 from dotenv import load_dotenv
-import certifi
 from pymongo import MongoClient
 
 load_dotenv()
 
 client = MongoClient(
-    os.getenv("MONGO_URL"),
-    tlsCAFile=certifi.where()
-)
+    os.getenv("MONGO_URL"))
 
 db = client["jungle-capsule"]
 
