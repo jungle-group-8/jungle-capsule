1f2626ce1d9fd80fc87918018d3ce5947d06d1d7
1224kang <cherrykang2003@gmail.com>
2026-08-26 14:36:05 +0900

    merging

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import random
 from routes import capmaking,capsulelist,checkcap,deletecap,detailcpa,member,question
 
+<<<<<<< HEAD
 client=MongoClient(os.getenv("MONGO_URL"))
 db=client["jungle-capsule"]
 collection=db["items"]
@@ -20,7 +21,12 @@ app.register_blueprint(deletecap.deletecap)
 app.register_blueprint(detailcpa.detailcap)
 app.register_blueprint(member.members)
 app.register_blueprint(question.question)
+=======
+app = Flask(__name__)
+
+>>>>>>> d894d8e (feat: 캡슐 생성 기능 연동 준비)
 
 
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5001, debug=True)
+   
